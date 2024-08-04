b672b61017eb66aa9c87c5d063f8093e394603dd
creativecstudios <merganesh@gmail.com>
2024-08-04 15:49:13 +0530

    Initial Setup

---
+++ src/test/java/com/premg/zerodha/ZerodhaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.premg.zerodha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZerodhaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
